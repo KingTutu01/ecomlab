6927aa341c5639e048fba9d03372c5109244ae0b
KingTutu01 <95937251+KingTutu01@users.noreply.github.com>
2022-11-30 15:30:12 +0000

    Update db_cred.php

--- tasklabs2/settings/db_cred.php
+++ tasklabs2/settings/db_cred.php
@@ -1,7 +1,6 @@
 <?php
-define("DATABASE", "shoppn");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
-
+define("DATABASE", "heroku_146f62f5dafe873");
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME", "b27c94bd167a0d");
+define("PASSWD", "ce2e7054");
 ?>
